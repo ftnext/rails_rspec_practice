e5ec190a0edc9d829670cddfcb1f315a0bd056b9
ftnext <takuyafjp+develop@gmail.com>
2018-08-14 20:19:40 +0900

    [test] have_http_statusを使ったテストを実装
    * 以下にならい、URIにgetしてからresponseを確認するテストを追加した
    https://relishapp.com/rspec/rspec-rails/v/3-8/docs/matchers/have-http-status-matcher#using-in-a-request-spec

--- spec/requests/post_controller_spec.rb
+++ spec/requests/post_controller_spec.rb
@@ -2,17 +2,26 @@ require 'rails_helper'
 
 RSpec.describe "Post controller", :type => :request do
   describe "GET #index" do
-    it "returns http success"
+    it "returns http success" do
+      get "/posts/index"
+      expect(response).to have_http_status(:success)
+    end
     it "renders the :index template"
   end
 
   describe "GET #edit" do
-    it "returns http success"
+    it "returns http success" do
+      get "/posts/edit"
+      expect(response).to have_http_status(:success)
+    end
     it "renders the :edit template"
   end
 
   describe "GET #new" do
-    it "returns http success"
+    it "returns http success" do
+      get "/posts/new"
+      expect(response).to have_http_status(:success)
+    end
     it "renders the :new template"
   end
 
